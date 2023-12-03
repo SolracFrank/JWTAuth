9aab11dffc0c090ee87b837dc948f9a83b0ad708
SolracFrank <carlosfranciscoherrerafranco@gmail.com>
2023-12-03 15:58:05 -0600

    feat: register cqrs features

---
+++ JWTAuth/Application/features/Auth/Register/RegisterCommand.cs
@@ -0,0 +1,12 @@
+﻿using LanguageExt.Common;
+using MediatR;
+
+namespace Application.features.Auth.Register
+{
+    public class RegisterCommand : IRequest<Result<string>>
+    {
+        public string Fullname { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ JWTAuth/Application/features/Auth/Register/RegisterCommandHandler.cs
@@ -0,0 +1,30 @@
+﻿using Application.Interfaces.Auth;
+using LanguageExt.Common;
+using MediatR;
+using ToklenAPI.Models.Dtos;
+
+namespace Application.features.Auth.Register
+{
+    public class RegisterCommandHandler : IRequestHandler<RegisterCommand, Result<string>>
+    {
+        private readonly IAuthService _authService;
+
+        public RegisterCommandHandler(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        public async Task<Result<string>> Handle(RegisterCommand request, CancellationToken cancellationToken)
+        {
+            var regsterDto = new UserRegisterDto
+            {
+                Fullname = request.Fullname,
+                Email = request.Email,
+                Password = request.Password,
+            };
+            var result = await _authService.Register(regsterDto, cancellationToken);
+
+            return new Result<string>(result);
+        }
+    }
+}
