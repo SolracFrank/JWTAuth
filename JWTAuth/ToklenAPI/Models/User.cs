aee57d38865cd0c64820afd646ef4e656ccd2311
SolracFrank <carlosfranciscoherrerafranco@gmail.com>
2023-12-02 22:18:18 -0600

    add: user entity

---
+++ JWTAuth/ToklenAPI/Models/User.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ToklenAPI.Models
+{
+    public class User
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(100)]
+        public string Fullname { get; set; }
+        [Required]
+        [EmailAddress(ErrorMessage = "Email invalid")]
+        [StringLength(100)]
+
+        public string Email { get; set; }
+        [Required]
+        [RegularExpression(@"^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*()_+\-=\[\]{};':\\|,.<>\/?]).{6,}$", ErrorMessage = "Password should have: alteast min 6 characters, one uppercase, one number and one especial character")]
+        public string Password { get; set; }
+    }
+}
