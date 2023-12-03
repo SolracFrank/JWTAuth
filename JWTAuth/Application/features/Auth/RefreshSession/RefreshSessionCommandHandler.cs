c32dc8bce0f8cc2b1c8892de4c1baf2948825266
SolracFrank <carlosfranciscoherrerafranco@gmail.com>
2023-12-03 15:58:18 -0600

    feat: refresh session cqrs features

---
+++ JWTAuth/Application/features/Auth/RefreshSession/RefreshSessionCommandHandler.cs
@@ -0,0 +1,23 @@
+﻿using Application.Interfaces.Auth;
+using LanguageExt.Common;
+using MediatR;
+using ToklenAPI.Models.Dtos.JWTToken;
+
+namespace Application.features.Auth.RefreshSession
+{
+    public class RefreshSessionCommandHandler : IRequestHandler<RefreshSessionCommand, Result<JWTResult>>
+    {
+        private readonly IAuthService _authService;
+
+        public RefreshSessionCommandHandler(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        public async Task<Result<JWTResult>> Handle(RefreshSessionCommand request, CancellationToken cancellationToken)
+        {
+            var result = await _authService.RefreshSessionToken(request.UserId, cancellationToken);
+            return new Result<JWTResult>(result);
+        }
+    }
+}
