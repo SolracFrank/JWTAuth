1231871a199c4180d5cca882fa4e4d2c6bf6c19a
SolracFrank <carlosfranciscoherrerafranco@gmail.com>
2023-12-03 12:06:10 -0600

    add: user login entities

---
+++ JWTAuth/ToklenAPI/Models/Dtos/JWTToken/JWTSettings.cs
@@ -0,0 +1,11 @@
+﻿namespace ToklenAPI.Models.Dtos.JWTToken
+{
+    public class JWTSettings
+    {
+        public string Issuer { get; set; }
+        public string Audience { get; set; }
+        public string Subject { get; set; }
+        public string Key { get; set; }
+        public int Duration { get; set; }
+    }
+}
---
+++ JWTAuth/ToklenAPI/Models/Dtos/UserLoginDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace ToklenAPI.Models.Dtos
+{
+    public class UserLoginDto
+    {
+        [EmailAddress(ErrorMessage = "Email invalid")]
+        [StringLength(100)]
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+}
