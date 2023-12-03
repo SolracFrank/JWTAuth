d4a79e784898f70e9bf7bfca2ec67d8cf38dc823
SolracFrank <carlosfranciscoherrerafranco@gmail.com>
2023-12-02 21:49:04 -0600

    add: base asp.net project

---
+++ JWTAuth/ToklenAPI/Controllers/AuthController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+
+namespace ToklenAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+       
+    }
+}
