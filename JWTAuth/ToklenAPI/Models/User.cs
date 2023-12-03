31b76cb9c72a611f28c69d340bd28918513eaff5
SolracFrank <carlosfranciscoherrerafranco@gmail.com>
2023-12-03 10:51:43 -0600

    feat: modify user entity fields

--- JWTAuth/ToklenAPI/Models/User.cs
+++ JWTAuth/ToklenAPI/Models/User.cs
@@ -1,20 +1,28 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ToklenAPI.Models
 {
+    [Table("BasicUser")]
     public class User
     {
-        [Required]
+        [Column("id")]
+        [Key]
         public int Id { get; set; }
         [Required]
         [StringLength(100)]
+        [Column("full_name")]
         public string Fullname { get; set; }
         [Required]
+        [Column("email")]
+
         [EmailAddress(ErrorMessage = "Email invalid")]
         [StringLength(100)]
 
         public string Email { get; set; }
         [Required]
+        [Column("password")]
+
         [RegularExpression(@"^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*()_+\-=\[\]{};':\\|,.<>\/?]).{6,}$", ErrorMessage = "Password should have: alteast min 6 characters, one uppercase, one number and one especial character")]
         public string Password { get; set; }
     }
