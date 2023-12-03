d76467d3a58b006e86326777d440a744d35a1838
SolracFrank <carlosfranciscoherrerafranco@gmail.com>
2023-12-03 13:58:21 -0600

    refactor: reorganize file structure for architecture update

--- JWTAuth/Infrastructure/Repositories/UserRepository.cs
+++ JWTAuth/Infrastructure/Repositories/UserRepository.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
