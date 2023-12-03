7409004b06f70c7e04c320a33e7cdfc27e1e6df8
SolracFrank <carlosfranciscoherrerafranco@gmail.com>
2023-12-03 10:53:56 -0600

    add: user dto

---
+++ JWTAuth/ToklenAPI/Models/Dtos/UserRegisterDto.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace ToklenAPI.Models.Dtos
+{
+    public class UserRegisterDto
+    {
+        [Required]
+        [StringLength(100)]
+        [Column("full_name")]
+        public string Fullname { get; set; }
+        [Required]
+        [Column("email")]
+
+        [EmailAddress(ErrorMessage = "Email invalid")]
+        [StringLength(100)]
+
+        public string Email { get; set; }
+        [Required]
+        [Column("password")]
+
+        [RegularExpression(@"^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*()_+\-=\[\]{};':\\|,.<>\/?]).{6,}$", ErrorMessage = "Password should have: alteast min 6 characters, one uppercase, one number and one especial character")]
+        public string Password { get; set; }
+    }
+}
