da03103b7334c6fa18f13b11b709f9891d1e2a72
SolracFrank <carlosfranciscoherrerafranco@gmail.com>
2023-12-03 14:58:18 -0600

    add: Global Exception Filter to controllers

--- JWTAuth/ToklenAPI/Program.cs
+++ JWTAuth/ToklenAPI/Program.cs
@@ -1,4 +1,5 @@
 using Infrastructure;
+using ToklenAPI.Filters;
 
 var builder = WebApplication.CreateBuilder(args);
 var isDevelopment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == Environments.Development;
@@ -9,7 +10,10 @@ var isDevelopment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")
 //add infrastructure
 builder.Services.AddInfrastructure(builder.Configuration);
 
-builder.Services.AddControllers();
+builder.Services.AddControllers(options =>
+{
+    options.Filters.Add<GlobalExceptionFilter>();
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
