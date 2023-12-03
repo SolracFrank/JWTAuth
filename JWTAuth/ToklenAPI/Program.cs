d2e0117cf393cdda197cf90345e8bee76a01308c
SolracFrank <carlosfranciscoherrerafranco@gmail.com>
2023-12-03 10:53:08 -0600

    feat: db context dependency injections

--- JWTAuth/ToklenAPI/Program.cs
+++ JWTAuth/ToklenAPI/Program.cs
@@ -1,11 +1,46 @@
+using Microsoft.EntityFrameworkCore;
+using ToklenAPI.Data;
+using ToklenAPI.Interfaces;
+using ToklenAPI.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
+var isDevelopment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == Environments.Development;
+
 
 // Add services to the container.
+
+//Add DBContext 
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"),
+    builder =>
+    {
+        builder.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName);
+        builder.EnableRetryOnFailure(4, TimeSpan.FromSeconds(5), null);
+    })
+);
+
+builder.Services.AddTransient<IUserRepository, UserRepository>();
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddHttpContextAccessor();
+
+if (isDevelopment)
+{
+    builder.Services.AddCors(options =>
+    {
+        options.AddPolicy("AllowAll", policyBuilder =>
+        {
+            policyBuilder.AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader();
+        });
+    });
+}
 
 var app = builder.Build();
 
