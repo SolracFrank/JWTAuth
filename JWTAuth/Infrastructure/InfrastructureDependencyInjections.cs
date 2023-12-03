eee6a17af6a31c4766d0085589226124ffa83ec8
SolracFrank <carlosfranciscoherrerafranco@gmail.com>
2023-12-03 13:58:34 -0600

    feat: refactor dependency injections

---
+++ JWTAuth/Infrastructure/InfrastructureDependencyInjections.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using ToklenAPI.Data;
+using ToklenAPI.Interfaces;
+using ToklenAPI.Models.Dtos.JWTToken;
+using ToklenAPI.Repositories;
+
+namespace Infrastructure
+{
+    public static class InfrastructureDependencyInjections
+    {
+        public static void AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            //Add DBContext 
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"),
+                builder =>
+                {
+                    builder.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName);
+                    builder.EnableRetryOnFailure(4, TimeSpan.FromSeconds(5), null);
+                })
+            );
+
+            //configure appsettings / usersecrets
+            services.Configure<JWTSettings>(configuration.GetSection("JwtSettings"));
+
+            //Add services
+           services.AddTransient<IUserRepository, UserRepository>();
+        }
+    }
+}
--- JWTAuth/ToklenAPI/Program.cs
+++ JWTAuth/ToklenAPI/Program.cs
@@ -1,8 +1,4 @@
-using Microsoft.EntityFrameworkCore;
-using ToklenAPI.Data;
-using ToklenAPI.Interfaces;
-using ToklenAPI.Models.Dtos.JWTToken;
-using ToklenAPI.Repositories;
+using Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 var isDevelopment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == Environments.Development;
@@ -10,19 +6,8 @@ var isDevelopment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")
 
 // Add services to the container.
 
-//Add DBContext 
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"),
-    builder =>
-    {
-        builder.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName);
-        builder.EnableRetryOnFailure(4, TimeSpan.FromSeconds(5), null);
-    })
-);
-builder.Services.Configure<JWTSettings>(builder.Configuration.GetSection("JwtSettings"));
-
-builder.Services.AddTransient<IUserRepository, UserRepository>();
-
+//add infrastructure
+builder.Services.AddInfrastructure(builder.Configuration);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
