474c3503cd86697f668a3916c5e38031cd6f3001
SolracFrank <carlosfranciscoherrerafranco@gmail.com>
2023-12-04 13:13:43 -0600

    add: identity user dtos

---
+++ JWTAuth/Domain/Models/Dtos/IdentityDtos/IdentityUserLoginDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Domain.Models.Dtos.IdentityDtos
+{
+    public class IdentityUserLoginDto
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+    }
+}
---
+++ JWTAuth/Domain/Models/Dtos/IdentityDtos/IdentityUserRegisterDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain.Models.Dtos.IdentityDtos
+{
+    public class IdentityUserRegisterDto
+    {
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string Password { get; set; }
+    }
+}
